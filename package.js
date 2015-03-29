d957cba0858d9942635100e2868205fa8f9e8355
thomasvanlankveld <thomas.van.lankveld@gmail.com>
2015-03-29 20:10:12 +0200

    Added description and git repo references to package.js

--- package.js
+++ package.js
@@ -2,14 +2,15 @@
 Package.describe({
   summary: "Adds a red border to the HTML body when in development environment",
   version: "1.0.0",
-  name: "thomasvanlankveld:meteor-envy"
-  // git: "https://github.com/something/something.git",
+  name: "thomasvanlankveld:meteor-envy",
+  documentation: "readme.md",
+  git: "https://github.com/thomasvanlankveld/meteor-envy.git"
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom('0.9.0');
-  api.use('underscore', 'server');
-  api.use('mrt:allow-env');
-  api.addFiles('client.js', 'client');
-  api.addFiles('server.js', 'server');
+  api.versionsFrom("0.9.0");
+  api.use("underscore", "server");
+  api.use("mrt:allow-env");
+  api.addFiles("client.js", "client");
+  api.addFiles("server.js", "server");
 });
